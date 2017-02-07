3a9b4360f393b0fcaa6f822efe77f7f52ed4084e
zhoujian <1064791247@qq.com>
2017-02-07 18:08:39 +0800

    zeke  学习https有关知识

---
+++ app/src/main/java/com/zhoujian/https/HttpsUtil.java
@@ -0,0 +1,110 @@
+package com.zhoujian.https;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import android.annotation.SuppressLint;
+
+public class HttpsUtil {
+	/**
+	 *
+	 * 
+	 * @param inStream
+	 * @return SSLContext
+	 * @throws NoSuchAlgorithmException
+	 * @throws IOException
+	 * @throws CertificateException
+	 * @throws KeyStoreException
+	 * @throws UnrecoverableKeyException
+	 * @throws KeyManagementException
+	 */
+	public static SSLContext getSSLContextWithCer() throws NoSuchAlgorithmException, IOException, CertificateException,
+			KeyStoreException, UnrecoverableKeyException, KeyManagementException {
+
+		SSLContext sslContext = SSLContext.getInstance("SSL");
+
+
+		InputStream inStream = MyApplication.getApplication().getAssets().open("zhoujian.cer");
+
+		CertificateFactory cerFactory = CertificateFactory.getInstance("X.509");
+		Certificate cer = cerFactory.generateCertificate(inStream);
+
+
+		KeyStore keyStory = KeyStore.getInstance("PKCS12");
+
+		keyStory.load(null, null);
+
+		keyStory.setCertificateEntry("ass", cer);
+
+
+		KeyManagerFactory kMFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		kMFactory.init(keyStory, null);
+
+		TrustManagerFactory tmFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		tmFactory.init(keyStory);
+
+
+		sslContext.init(kMFactory.getKeyManagers(), tmFactory.getTrustManagers(), new SecureRandom());
+		inStream.close();
+		return sslContext;
+
+	}
+
+	/**
+	 *
+	 * 
+	 * @return SSLContext
+	 * @throws NoSuchAlgorithmException
+	 * @throws KeyManagementException
+	 */
+	@SuppressLint("TrulyRandom")
+	public static SSLContext getSSLContextWithoutCer() throws NoSuchAlgorithmException, KeyManagementException {
+
+		SSLContext sslContext = SSLContext.getInstance("SSL");
+		sslContext.init(null, new TrustManager[] { trustManagers }, new SecureRandom());
+		return sslContext;
+
+	}
+
+	private static TrustManager trustManagers = new X509TrustManager() {
+
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		}
+
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+		}
+
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			return new X509Certificate[0];
+		}
+	};
+
+	public static HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+
+		@Override
+		public boolean verify(String hostname, SSLSession session) {
+			// TODO Auto-generated method stub
+			return true;
+		}
+	};
+}
---
+++ app/src/main/java/com/zhoujian/https/MainActivity.java
@@ -0,0 +1,71 @@
+package com.zhoujian.https;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Thread() {
+            public void run() {
+                try {
+                    getConnection();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+
+    private void getConnection() throws IOException, KeyManagementException, NoSuchAlgorithmException,
+            UnrecoverableKeyException, CertificateException, KeyStoreException {
+
+        // https://github.com/zeke123/ConstraintLayout
+        // http://my.csdn.net/my/mycsdn
+        URL url = new URL("https://github.com/zeke123/ConstraintLayout");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setConnectTimeout(5 * 1000);
+        connection.setReadTimeout(5 * 1000);
+        connection.setRequestMethod("GET");
+
+        Log.e("zhoujian", "url==" + url);
+        Log.e("zhoujian", " 是否是https请求==" + (connection instanceof HttpsURLConnection));
+        if (connection instanceof HttpsURLConnection) {
+            SSLContext sslContext = HttpsUtil.getSSLContextWithCer();
+			 //SSLContext sslContext = HttpsUtil.getSSLContextWithoutCer();
+            if (sslContext != null) {
+                SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+               ((HttpsURLConnection) connection).setDefaultSSLSocketFactory(sslSocketFactory);
+				//((HttpsURLConnection) connection).setHostnameVerifier(HttpsUtil.hostnameVerifier);
+            }
+        }
+
+        int responseCode = connection.getResponseCode();
+        Log.e("zhoujian", "responseCode==" + responseCode);
+        if (responseCode == 200) {
+            InputStream is = connection.getInputStream();
+            Log.e("zhoujian", "is==" + is);
+            is.close();
+        }
+        connection.disconnect();
+    }
+}
+
