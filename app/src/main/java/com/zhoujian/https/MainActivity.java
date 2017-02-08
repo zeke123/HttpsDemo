39dd80a4d8c6d7e1d5ced94c3d6c3db1cac24bfa
zhoujian <1064791247@qq.com>
2017-02-08 10:28:41 +0800

    zeke  提交代码

--- app/src/main/java/com/zhoujian/https/MainActivity.java
+++ app/src/main/java/com/zhoujian/https/MainActivity.java
@@ -20,9 +20,11 @@ import javax.net.ssl.SSLSocketFactory;
 public class MainActivity extends Activity
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         new Thread()
         {
             public void run()
