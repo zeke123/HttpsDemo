5247f677b944a48333540733d113d3b103d065b5
zhoujian <1064791247@qq.com>
2017-02-09 15:16:50 +0800

    zeke  提交代码

--- app/src/main/java/com/zhoujian/https/MainActivity.java
+++ app/src/main/java/com/zhoujian/https/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends Activity
                 }
             }
         }.start();
+
     }
 
     private void getConnection() throws IOException, KeyManagementException, NoSuchAlgorithmException,
